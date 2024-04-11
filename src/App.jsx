3c5cd0e9c33fbca7fd6e8ad6e34b4c215e3507bb
amponsem michael <amponsem_michael@yahoo.com>
2024-04-11 06:38:42 +0000

    'commitng changes with dynamic json'

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import HomeCards from "./components/HomeCards";
-import JobListings from "./components/JobListings";
+import {
+  Navbar,
+  Hero,
+  HomeCards,
+  JobListings,
+  ViewAllJobs,
+} from "./components/index";
+
 const App = () => {
   return (
     <>
@@ -9,15 +13,7 @@ const App = () => {
       <Hero />
       <HomeCards />
       <JobListings />
-
-      <section className="m-auto max-w-lg my-10 px-6">
-        <a
-          href="jobs.html"
-          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >
-          View All Jobs
-        </a>
-      </section>
+      <ViewAllJobs />
     </>
   );
 };
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,5 @@
 const Card = ({ children, bg = "bg-gray-100" }) => {
-  return <div className={`${bg} p-6 rounded-lg shadow-md`}>{children} </div>;
+  return <div className={`${bg} p-6 rounded-lg shadow-md`}> {children} </div>;
 };
 
 export default Card;
