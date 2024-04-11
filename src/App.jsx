399db5fc700c3c31fcbe3296fb45f19cdab0edfc
amponsem michael <amponsem_michael@yahoo.com>
2024-04-11 23:57:44 +0000

    "commiting project after including react-router-dom"

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
 import {
-  Navbar,
-  Hero,
-  HomeCards,
-  JobListings,
-  ViewAllJobs,
-} from "./components/index";
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import MainLayout from "./layouts/MainLayout";
+import JobsPage from "./pages/JobsPage";
+import NotFoundPage from "./pages/NotFoundPage";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path="/jobs" element={<JobsPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Route>
+  )
+);
 
 const App = () => {
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <HomeCards />
-      <JobListings />
-      <ViewAllJobs />
-    </>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,16 +1,17 @@
 import JobListing from "./JobListing";
 import jobs from "../jobs.json";
 
-const JobListings = () => {
-  const recentJobs = jobs.slice(0, 3);
+const JobListings = ({ isHome = false }) => {
+  const jobListings = isHome ? jobs.slice(0, 3) : jobs;
   return (
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
         <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-          Browse Jobs
+          {isHome ? "Recent Jobs" : "Browse Jobs"}
         </h2>
+
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {recentJobs.map((job) => (
+          {jobListings.map((job) => (
             <JobListing key={job.id} job={job} />
           ))}
         </div>
