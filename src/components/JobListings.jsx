feb4bc85c4df5e04fde45058dc45c929fddbbb06
amponsem michael <amponsem_michael@yahoo.com>
2024-04-13 07:23:48 +0000

    'committing job with the inclusion of useLoaderData'

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,8 +1,28 @@
+import { useState, useEffect } from "react";
 import JobListing from "./JobListing";
-import jobs from "../jobs.json";
+import Spinner from "./Spinner";
 
 const JobListings = ({ isHome = false }) => {
-  const jobListings = isHome ? jobs.slice(0, 3) : jobs;
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      const apiURL = isHome ? "/api/jobs?_limit=3" : "/api/jobs";
+      try {
+        const res = await fetch(apiURL);
+        const data = await res.json();
+        setJobs(data);
+      } catch (error) {
+        console.log("Error fetching data", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchJobs();
+  }, []);
+
   return (
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
@@ -10,11 +30,15 @@ const JobListings = ({ isHome = false }) => {
           {isHome ? "Recent Jobs" : "Browse Jobs"}
         </h2>
 
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {jobListings.map((job) => (
-            <JobListing key={job.id} job={job} />
-          ))}
-        </div>
+        {loading ? (
+          <Spinner loading={loading} />
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {jobs.map((job) => (
+              <JobListing key={job.id} job={job} />
+            ))}
+          </div>
+        )}
       </div>
     </section>
   );
